c74b01724ffcbaab09dae74756b620ac4b8bfb3e
MahmoudElwsef <mahmoud.elwsef.0.0@gmail.com>
2020-07-06 05:58:13 -0700

    initial commit

---
+++ ProductDetails/Source Code.Designer.vb
@@ -0,0 +1,242 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.BtnNewProduct = New System.Windows.Forms.Button()
+        Me.BtnDeleteProduct = New System.Windows.Forms.Button()
+        Me.BtnUpdateProduct = New System.Windows.Forms.Button()
+        Me.BtnViewProduct = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Snow
+        Me.Label1.Font = New System.Drawing.Font("Snap ITC", 18.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.Location = New System.Drawing.Point(374, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(302, 39)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Product Details"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(21, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(120, 24)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Product ID"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(390, 108)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(153, 24)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Product Name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(21, 195)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(180, 24)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Current Quantity"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(390, 195)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(109, 24)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Unit Price"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(733, 196)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(133, 24)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Dealer Price"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(209, 108)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(155, 23)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(556, 109)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(155, 23)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(209, 195)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(155, 23)
+        Me.TextBox3.TabIndex = 8
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(556, 195)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(155, 23)
+        Me.TextBox4.TabIndex = 9
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(882, 195)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(155, 23)
+        Me.TextBox5.TabIndex = 10
+        '
+        'BtnNewProduct
+        '
+        Me.BtnNewProduct.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.BtnNewProduct.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnNewProduct.Location = New System.Drawing.Point(28, 320)
+        Me.BtnNewProduct.Name = "BtnNewProduct"
+        Me.BtnNewProduct.Size = New System.Drawing.Size(218, 44)
+        Me.BtnNewProduct.TabIndex = 11
+        Me.BtnNewProduct.Text = "New Product"
+        Me.BtnNewProduct.UseVisualStyleBackColor = False
+        '
+        'BtnDeleteProduct
+        '
+        Me.BtnDeleteProduct.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.BtnDeleteProduct.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnDeleteProduct.Location = New System.Drawing.Point(289, 320)
+        Me.BtnDeleteProduct.Name = "BtnDeleteProduct"
+        Me.BtnDeleteProduct.Size = New System.Drawing.Size(218, 44)
+        Me.BtnDeleteProduct.TabIndex = 12
+        Me.BtnDeleteProduct.Text = "Delete Product"
+        Me.BtnDeleteProduct.UseVisualStyleBackColor = False
+        '
+        'BtnUpdateProduct
+        '
+        Me.BtnUpdateProduct.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.BtnUpdateProduct.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnUpdateProduct.Location = New System.Drawing.Point(553, 320)
+        Me.BtnUpdateProduct.Name = "BtnUpdateProduct"
+        Me.BtnUpdateProduct.Size = New System.Drawing.Size(218, 44)
+        Me.BtnUpdateProduct.TabIndex = 13
+        Me.BtnUpdateProduct.Text = "Update Product"
+        Me.BtnUpdateProduct.UseVisualStyleBackColor = False
+        '
+        'BtnViewProduct
+        '
+        Me.BtnViewProduct.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.BtnViewProduct.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnViewProduct.Location = New System.Drawing.Point(817, 320)
+        Me.BtnViewProduct.Name = "BtnViewProduct"
+        Me.BtnViewProduct.Size = New System.Drawing.Size(218, 44)
+        Me.BtnViewProduct.TabIndex = 14
+        Me.BtnViewProduct.Text = "View Product"
+        Me.BtnViewProduct.UseVisualStyleBackColor = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(52, 412)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 26
+        Me.DataGridView1.Size = New System.Drawing.Size(927, 276)
+        Me.DataGridView1.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1069, 714)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.BtnViewProduct)
+        Me.Controls.Add(Me.BtnUpdateProduct)
+        Me.Controls.Add(Me.BtnDeleteProduct)
+        Me.Controls.Add(Me.BtnNewProduct)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Tahoma", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents BtnNewProduct As Button
+    Friend WithEvents BtnDeleteProduct As Button
+    Friend WithEvents BtnUpdateProduct As Button
+    Friend WithEvents BtnViewProduct As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ ProductDetails/Source Code.vb
@@ -0,0 +1,163 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim conn As SqlConnection
+    Dim da As SqlDataAdapter
+    Dim ds As DataSet
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Make connection to The SQL Server
+        Dim strConnection As String = "Data Source=DESKTOP-MSMKMEG\ELWASEFSERVER ; initial Catalog=master ; integrated Security=true"
+        conn = New SqlConnection(strConnection)
+        conn.Open()
+
+        'This outer Try used to Check Database 'ProductDB' exist or Not
+        Try
+            conn.ChangeDatabase("ProductDB") 'Change connection From Master to ProductDB
+
+            'if Database 'ProductDB' Not exist
+        Catch ex1 As Exception
+            MsgBox(ex1.Message)
+
+            Try
+                'Create Database 'ProductDB'
+                Dim command1 As New SqlCommand("Create database ProductDB", conn)
+                command1.ExecuteNonQuery() 'To execute Query
+
+                'Change connection From Master to ProductDB
+                command1.Connection.ChangeDatabase("ProductDB")
+
+                'Create Table 'Products'
+                Dim command2 As New SqlCommand("Create table Products(ID int , name char(30) , Quantity int , unit_Price int , delar_Price int)", conn)
+                command2.ExecuteNonQuery() 'To execute Query
+
+            Catch ex2 As Exception
+                MsgBox(ex2.Message)
+
+            End Try
+
+        End Try
+
+        'To get Data From Database to DataAdapter
+        da = New SqlDataAdapter("select * from Products", conn)
+
+        'To Reconcile DataAdapter with DataSet
+        Dim cb As New SqlCommandBuilder(da)
+
+        'To Fill Data in Dataset From DataAdapter and give it a Name (This Name to Table in DataSet)
+        ds = New DataSet()
+        da.Fill(ds, "Products")
+
+        'view Data in DataGridView From DataSet
+        DataGridView1.DataBindings.Add("DataSource", ds, "Products")
+
+    End Sub
+
+    'This Button Used To Save data From TextBoxes to Original Database
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnNewProduct.Click
+
+        'make NewRow in Table 'Products' in DataSet
+        Dim r As DataRow = ds.Tables("Products").NewRow()
+
+        r("ID") = Val(TextBox1.Text)                    ' r(0) = Val(TextBox1.Text)
+        r("name") = TextBox2.Text                       ' r(1) = TextBox2.Text
+        r("Quantity") = TextBox3.Text                   ' r(2) = Val(TextBox3.Text)
+        r("unit_Price") = Val(TextBox4.Text)            ' r(3) = Val(TextBox4.Text)
+        r("delar_Price") = Val(TextBox5.Text)           ' r(4) = Val(TextBox5.Text)
+
+        'Add Row in Table 'Products' in DataSet
+        ds.Tables("Products").Rows.Add(r)
+
+        'Add Row in Table 'Products' in DataAdapter(To affect the original Database)
+        da.Update(ds, "Products")
+
+    End Sub
+
+    Private Sub BtnViewProduct_Click(sender As Object, e As EventArgs) Handles BtnViewProduct.Click
+
+        'get Spacific Row From Database
+        Dim command1 As New SqlCommand("Select * from Products where ID=" & Val(TextBox1.Text), conn)
+
+        Try
+            'execute and Reade Query
+            Dim Reader As SqlDataReader = command1.ExecuteReader()
+            Reader.Read()
+
+            'show Row in TextBoxes
+            TextBox2.Text = Reader(1)
+            TextBox3.Text = Reader(2)
+            TextBox4.Text = Reader(3)
+            TextBox5.Text = Reader(4)
+
+            'close connection because ..
+            '    conn.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        End Try
+
+    End Sub
+
+    Private Sub BtnDeleteProduct_Click(sender As Object, e As EventArgs) Handles BtnDeleteProduct.Click
+
+        Dim answer As Integer
+        Dim r As DataRow = Nothing
+
+        For Each r In ds.Tables("Products").Rows
+
+            If r("ID") = Val(TextBox1.Text) Then
+
+                'show Row that user want to Delete in TextBoxes
+                TextBox2.Text = r("name")
+                TextBox3.Text = r("Quantity")
+                TextBox4.Text = r("unit_Price")
+                TextBox5.Text = r("delar_Price")
+
+                'if we Found ID that user want to Delete
+                Exit For
+
+            End If
+
+        Next
+
+        answer = MsgBox("Are You Sure you Want To delete This Product?", MsgBoxStyle.OkCancel)
+        '1 --> Yes
+        '2 --> No
+
+        If answer = 2 Then Exit Sub
+
+        r.Delete()
+        da.Update(ds, "Products")
+
+    End Sub
+
+    Private Sub BtnUpdateProduct_Click(sender As Object, e As EventArgs) Handles BtnUpdateProduct.Click
+
+        Dim r As DataRow = Nothing
+
+        For Each r In ds.Tables("Products").Rows
+
+            If r("ID") = Val(TextBox1.Text) Then
+
+                'get Updates From TextBoxes to Row 'r'
+                r("name") = TextBox2.Text
+                r("Quantity") = TextBox3.Text
+                r("unit_Price") = TextBox4.Text
+                r("delar_Price") = TextBox5.Text
+
+                'if we Found ID that user want to Update
+                Exit For
+
+            End If
+
+        Next
+
+        da.Update(ds, "Products")
+
+    End Sub
+
+End Class
+
